8a46a07f42abb0e296ebff956ea6d8e67932f315
Abhinav Niraula <aavinavkaji@gmail.com>
2019-11-24 16:08:42 +0545

    packagesInit

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+app.get("/test", (req, res) => {
+    res.json({
+      message: "Welcome to service app."
+    });
+});
+
+// app.use(
+//     "/graphql",
+//     graphqlHttp({
+//       schema: graphqlSchema,
+//       rootValue: graphqlResolver,
+//       graphiql: true
+//     })
+// );
+
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
+    console.log(`server started at http://localhost:${port}`)
+});
